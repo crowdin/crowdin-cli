a6e25f4ba1b05961ac9762cf2d08df824967a1c0
Barabash Daniil <dBarabash42@gmail.com>
2020-02-04 14:29:48 +0200

    Fix tree view

--- src/main/java/com/crowdin/cli/commands/functionality/Dryrun.java
+++ src/main/java/com/crowdin/cli/commands/functionality/Dryrun.java
@@ -1,17 +1,22 @@
 package com.crowdin.cli.commands.functionality;
 
 import com.crowdin.cli.utils.tree.DrawTree;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class Dryrun {
 
     protected abstract List<String> getFiles();
 
     public void run(boolean treeView) {
-        List<String> files = getFiles();
-        Collections.sort(files);
+        List<String> files = getFiles()
+            .stream()
+            .map(f -> f.replaceAll("^[/\\\\]+", ""))
+            .sorted()
+            .collect(Collectors.toList());
         if (treeView) {
             (new DrawTree()).draw(files, 0);
         } else {
