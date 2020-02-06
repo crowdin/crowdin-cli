c2c3f898e4c4dc3dbd70a38df7935cbf950c5861
fromBetelgeuse <dBarabash42@gmail.com>
2020-02-07 00:47:06 +0200

    Fix translation_replace for translations dryrun

--- src/main/java/com/crowdin/cli/commands/functionality/DryrunTranslations.java
+++ src/main/java/com/crowdin/cli/commands/functionality/DryrunTranslations.java
@@ -3,9 +3,10 @@ package com.crowdin.cli.commands.functionality;
 import com.crowdin.cli.properties.PropertiesBean;
 import com.crowdin.cli.utils.CommandUtils;
 import com.crowdin.cli.utils.PlaceholderUtil;
+import com.crowdin.cli.utils.Utils;
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.File;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,7 +29,16 @@ public class DryrunTranslations extends Dryrun {
                 .map(source -> placeholderUtil.replaceFileDependentPlaceholders(file.getTranslation(), source))
                 .flatMap(translation -> ((file.getLanguagesMapping() != null)
                     ? placeholderUtil.replaceLanguageDependentPlaceholders(translation, file.getLanguagesMapping())
-                    : placeholderUtil.replaceLanguageDependentPlaceholders(translation)).stream()))
+                    : placeholderUtil.replaceLanguageDependentPlaceholders(translation)).stream())
+                .map(translation -> (file.getTranslationReplace() != null ? file.getTranslationReplace() : Collections.<String, String>emptyMap())
+                    .keySet()
+                    .stream()
+                    .reduce(translation, (trans, k) -> StringUtils.replace(
+                        trans,
+                        k.replaceAll("[\\\\/]+", Utils.PATH_SEPARATOR_REGEX),
+                        file.getTranslationReplace().get(k)))
+                )
+            )
             .map(source -> StringUtils.removeStart(source, pb.getBasePath()))
             .collect(Collectors.toList());
     }
