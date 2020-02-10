94fa4c91c4a1139913c25be0665071654d49620d
Barabash Daniil <dBarabash42@gmail.com>
2020-02-10 17:35:42 +0200

    Improve init - add defaults

--- src/main/java/com/crowdin/cli/commands/GenerateSubcommand.java
+++ src/main/java/com/crowdin/cli/commands/GenerateSubcommand.java
@@ -2,19 +2,19 @@ package com.crowdin.cli.commands;
 
 import com.crowdin.cli.commands.parts.Command;
 import com.crowdin.cli.utils.console.ExecutionStatus;
-import com.crowdin.cli.utils.file.FileUtil;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import picocli.CommandLine;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.Map;
 import java.util.Scanner;
 
 import static com.crowdin.cli.properties.CliProperties.*;
-import static com.crowdin.cli.utils.MessageSource.Messages.GENERATE_HELP_MESSAGE;
 
 @CommandLine.Command(
     name = "generate",
@@ -29,6 +29,12 @@ public class GenerateSubcommand extends Command {
     @CommandLine.Option(names = "--skip-generate-description", hidden = true)
     private boolean skipGenerateDescription;
 
+    public static final String BASE_PATH_DEFAULT = ".";
+    public static final String BASE_URL_DEFAULT = "https://api.crowdin.com";
+    public static final String BASE_ENTERPRISE_URL_DEFAULT = "https://%s.crowdin.com";
+
+    private Scanner scanner = new Scanner(System.in);
+
     @Override
     public void run() {
         try {
@@ -38,42 +44,56 @@ public class GenerateSubcommand extends Command {
                 return;
             }
 
+            List<String> file;
             try {
-                FileUtil.writeToFile(this.getClass().getResourceAsStream("/crowdin.yml"), destinationPath.toString());
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException("The path specified doesn't contain");
+                file = IOUtils.readLines(this.getClass().getResourceAsStream("/crowdin.yml"), "UTF-8");
             } catch (IOException e) {
-                throw new RuntimeException("Couldn't create destination file", e);
+                throw new RuntimeException("Couldn't read from resource file", e);
             }
 
             if (!skipGenerateDescription) {
-                System.out.println(GENERATE_HELP_MESSAGE.getString());
-                List<String> dummyConfig = FileUtil.readFile(destinationPath.toFile());
+                Map<String, String> values = new HashMap<>();
 
-                Scanner consoleScanner = new Scanner(System.in);
-                String[] params = {API_TOKEN, PROJECT_ID, BASE_PATH, BASE_URL};
-                for (String param : params) {
-                    System.out.print(param.replaceAll("_", " ") + " : ");
-                    String userInput = consoleScanner.nextLine();
+                values.put(BASE_PATH, askParamWithDefault(BASE_PATH, BASE_PATH_DEFAULT));
+                values.put(BASE_URL,
+                        askParamWithDefault(BASE_URL,
+                                StringUtils.startsWithAny(ask("Is it enterprise: (N/y) "), "y", "Y", "+")
+                                        ? String.format(BASE_ENTERPRISE_URL_DEFAULT, ask("Enter your organization: "))
+                                        : BASE_URL_DEFAULT));
+                values.put(PROJECT_ID, askParam(PROJECT_ID));
+                values.put(API_TOKEN, askParam(API_TOKEN));
 
-                    ListIterator<String> dummyConfigIterator = dummyConfig.listIterator();
-                    while (dummyConfigIterator.hasNext()) {
-                        String defaultLine = dummyConfigIterator.next();
-                        if (defaultLine.contains(param)) {
-                            String lineWithUserInput = defaultLine.replaceFirst(": \"*\"", String.format(": \"%s\"", userInput));
-                            dummyConfigIterator.set(lineWithUserInput);
-                            try {
-                                Files.write(destinationPath, dummyConfig);
-                            } catch (IOException e) {
-                                throw new RuntimeException("Couldn't write to file '" + destinationPath.toAbsolutePath() + "'", e);
-                            }
+                for (String key : values.keySet()) {
+                    for (int i = 0; i < file.size(); i++) {
+                        if (file.get(i).contains(key)) {
+                            file.set(i, file.get(i).replaceFirst(": \"*\"", String.format(": \"%s\"", values.get(key))));
                             break;
                         }
                     }
                 }
             }
+
+            try {
+                Files.write(destinationPath, file);
+            } catch (IOException e) {
+                throw new RuntimeException("Couldn't write to file '" + destinationPath.toAbsolutePath() + "'", e);
+            }
         } catch (Exception e) {
             throw new RuntimeException("Error while creating config file", e);
         }
+    }
+
+    private String askParamWithDefault(String key, String def) {
+        String input = ask(StringUtils.capitalize(key.replaceAll("_", " ")) + ": (" + def + ") ");
+        return StringUtils.isNotEmpty(input) ? input : def;
+    }
+
+    private String askParam(String key) {
+        return ask(StringUtils.capitalize(key.replaceAll("_", " ")) + ": ");
+    }
+
+    private String ask(String question) {
+        System.out.print(question);
+        return scanner.nextLine();
     }
 }
