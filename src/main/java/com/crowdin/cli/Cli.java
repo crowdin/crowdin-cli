c82bdbbcc283a1c69d69203e21150b6233b173fc
Barabash Daniil <dBarabash42@gmail.com>
2020-02-27 11:52:56 +0200

    Add fetching crowdin-cli version

--- src/main/java/com/crowdin/cli/Cli.java
+++ src/main/java/com/crowdin/cli/Cli.java
@@ -2,30 +2,19 @@ package com.crowdin.cli;
 
 import com.crowdin.cli.commands.RootCommand;
 import com.crowdin.cli.commands.parts.HelpCommand;
+import com.crowdin.cli.utils.MessageSource;
+import com.crowdin.cli.utils.Utils;
 import picocli.CommandLine;
 
 public class Cli {
 
     public static void main(String[] args) {
         try {
-            CommandLine.Help.ColorScheme colorScheme = new CommandLine.Help.ColorScheme.Builder()
-                .commands(CommandLine.Help.Ansi.Style.fg_yellow)
-                .options(CommandLine.Help.Ansi.Style.fg_yellow)
-                .build();
-            CommandLine.IExecutionExceptionHandler executionExceptionHandler = (ex, cmd, pr) -> {
-                if (pr.originalArgs().contains("--debug")) {
-                    ex.printStackTrace();
-                } else {
-                    cmd.getErr().println(ex.getMessage());
-                    Throwable cause = ex;
-                    while ((cause = cause.getCause()) != null) {
-                        cmd.getErr().println(cause.getMessage());
-                    }
-                }
-                return cmd.getExitCodeExceptionMapper() != null
-                        ? cmd.getExitCodeExceptionMapper().getExitCode(ex)
-                        : cmd.getCommandSpec().exitCodeOnExecutionException();
-            };
+            Utils.getAppNewLatestVersion()
+                .ifPresent(newVersion -> System.out.println(String.format(MessageSource.RESOURCE_BUNDLE.getString("message.new_version_available"), newVersion)));
+
+            CommandLine.Help.ColorScheme colorScheme = buildColorScheme();
+            CommandLine.IExecutionExceptionHandler executionExceptionHandler = buildExecutionExceptionHandler();
             CommandLine commandLine = new CommandLine(new RootCommand())
                 .setExecutionExceptionHandler(executionExceptionHandler)
                 .setColorScheme(colorScheme);
@@ -36,5 +25,29 @@ public class Cli {
             System.out.println("There is exception:");
             e.printStackTrace();
         }
+    }
+
+    private static CommandLine.Help.ColorScheme buildColorScheme() {
+        return new CommandLine.Help.ColorScheme.Builder()
+                .commands(CommandLine.Help.Ansi.Style.fg_yellow)
+                .options(CommandLine.Help.Ansi.Style.fg_yellow)
+                .build();
+    }
+
+    private static CommandLine.IExecutionExceptionHandler buildExecutionExceptionHandler() {
+        return (ex, cmd, pr) -> {
+            if (pr.originalArgs().contains("--debug")) {
+                ex.printStackTrace();
+            } else {
+                cmd.getErr().println(ex.getMessage());
+                Throwable cause = ex;
+                while ((cause = cause.getCause()) != null) {
+                    cmd.getErr().println(cause.getMessage());
+                }
+            }
+            return cmd.getExitCodeExceptionMapper() != null
+                    ? cmd.getExitCodeExceptionMapper().getExitCode(ex)
+                    : cmd.getCommandSpec().exitCodeOnExecutionException();
+        };
     }
 }
