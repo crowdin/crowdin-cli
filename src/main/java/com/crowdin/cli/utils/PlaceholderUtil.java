2f4e9f1d9e51dc83bdbdd5b2ce77e3ee59ac6658
Barabash Daniil <dBarabash42@gmail.com>
2020-03-10 17:07:17 +0200

    Fix double asterisks with a slash at the start

--- src/main/java/com/crowdin/cli/utils/PlaceholderUtil.java
+++ src/main/java/com/crowdin/cli/utils/PlaceholderUtil.java
@@ -134,7 +134,7 @@ public class PlaceholderUtil {
                 .replace(PLACEHOLDER_FILE_NAME, fileNameWithoutExt)
                 .replace(PLACEHOLDER_FILE_EXTENTION, fileExt)
                 .replace(PLACEHOLDER_ORIGINAL_PATH, fileParent);
-        String doubleAsterisks = StringUtils.removeStart(fileParent, StringUtils.removeStart(StringUtils.substringBefore(toFormat, Utils.PATH_SEPARATOR + "**"), Utils.PATH_SEPARATOR));
+        String doubleAsterisks = Utils.PATH_SEPARATOR + StringUtils.removeStart(fileParent, StringUtils.removeStart(StringUtils.substringBefore(toFormat, Utils.PATH_SEPARATOR + "**"), Utils.PATH_SEPARATOR));
         return toFormat
                 .replace(Utils.PATH_SEPARATOR + "**", doubleAsterisks)
                 .replaceAll("[\\\\/]+", Utils.PATH_SEPARATOR_REGEX);
