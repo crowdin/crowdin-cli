6fb133d7ef331ce69fb0b2afc299ff85df7774f6
Barabash Daniil <dBarabash42@gmail.com>
2020-05-05 13:55:42 +0300

    Fix missing error messages

--- src/main/java/com/crowdin/cli/client/exceptions/UnauthorizedResponseException.java
+++
@@ -1,8 +0,0 @@
-package com.crowdin.cli.client.exceptions;
-
-public class UnauthorizedResponseException extends ResponseException {
-
-    public UnauthorizedResponseException() {
-        super();
-    }
-}
