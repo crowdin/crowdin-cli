8ba916fc26c875ac6fd0f5355fcaf95d01e76eeb
Barabash <dBarabash42@gmail.com>
2020-07-30 13:17:05 +0300

    Add new 'crowdin list branches' command (#300)

---
+++ src/main/java/com/crowdin/cli/commands/actions/ListBranchesAction.java
@@ -0,0 +1,40 @@
+package com.crowdin.cli.commands.actions;
+
+import com.crowdin.cli.client.Client;
+import com.crowdin.cli.client.Project;
+import com.crowdin.cli.commands.functionality.DryrunBranches;
+import com.crowdin.cli.properties.PropertiesBean;
+import com.crowdin.cli.utils.console.ConsoleSpinner;
+
+import static com.crowdin.cli.BaseCli.RESOURCE_BUNDLE;
+import static com.crowdin.cli.utils.console.ExecutionStatus.ERROR;
+import static com.crowdin.cli.utils.console.ExecutionStatus.OK;
+
+public class ListBranchesAction implements Action {
+
+    private boolean noProgress;
+    private boolean plainView;
+
+    public ListBranchesAction(boolean noProgress,  boolean plainView) {
+        this.noProgress = noProgress || plainView;
+        this.plainView = plainView;
+    }
+
+    @Override
+    public void act(PropertiesBean pb, Client client) {
+        Project project;
+        try {
+            if (!plainView) {
+                ConsoleSpinner.start(RESOURCE_BUNDLE.getString("message.spinner.fetching_project_info"), this.noProgress);
+            }
+            project = client.downloadFullProject();
+            ConsoleSpinner.stop(OK);
+        } catch (Exception e) {
+            ConsoleSpinner.stop(ERROR);
+            throw new RuntimeException(RESOURCE_BUNDLE.getString("error.collect_project_info"), e);
+        }
+
+        new DryrunBranches(project.getBranches())
+            .run(false, plainView);
+    }
+}
