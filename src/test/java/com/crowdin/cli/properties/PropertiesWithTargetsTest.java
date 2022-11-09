ff43676865aeb4a80bb5e38341976434e6d0395d
DukeRino7 <94042718+DukeRino7@users.noreply.github.com>
2022-11-09 20:35:13 +0100

    fix

--- src/test/java/com/crowdin/cli/properties/PropertiesWithTargetsTest.java
+++ src/test/java/com/crowdin/cli/properties/PropertiesWithTargetsTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 
 
-public class PropertiesWithTargetsBuilderTest {
+class PropertiesWithTargetsBuilderTest {
 
     private TempProject tempProject;
     private final Outputter out = Outputter.getDefault();
