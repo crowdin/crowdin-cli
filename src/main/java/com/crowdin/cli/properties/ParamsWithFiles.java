e5fe3695dc1b18c7bc5e2cee5da88bceae9b69a5
yzerk <zerkal.work@gmail.com>
2023-01-04 10:12:58 +0200

    #311 - fix review notes

--- src/main/java/com/crowdin/cli/properties/ParamsWithFiles.java
+++ src/main/java/com/crowdin/cli/properties/ParamsWithFiles.java
@@ -23,8 +23,6 @@ public class ParamsWithFiles extends ProjectParams {
     private Boolean skipUntranslatedFiles;
     private Boolean exportApprovedOnly;
 
-    private Boolean keepArchive;
-
     private List<String> labels;
 
     private List<String> excludedTargetLanguages;
