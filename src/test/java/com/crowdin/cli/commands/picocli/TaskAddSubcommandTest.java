b64a219a6aebb7c6aa4c27099406f01f38a9287c
yzerk <zerkal.work@gmail.com>
2023-01-20 02:49:41 +0200

    #261 - fix test on ci

--- src/test/java/com/crowdin/cli/commands/picocli/TaskAddSubcommandTest.java
+++ src/test/java/com/crowdin/cli/commands/picocli/TaskAddSubcommandTest.java
@@ -1,10 +1,15 @@
 package com.crowdin.cli.commands.picocli;
 
+import com.crowdin.cli.commands.Outputter;
+import com.crowdin.cli.properties.ProjectProperties;
+import com.crowdin.cli.properties.ProjectPropertiesBuilder;
 import com.crowdin.cli.properties.PropertiesBuilders;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
@@ -16,7 +21,7 @@ public class TaskAddSubcommandTest extends PicocliTestUtils {
     @Test
     public void testTaskAdd() {
         System.out.println("running execute command");
-        this.execute(CommandNames.TASK, CommandNames.TASK_ADD, "Test Task1", "--language", "es", "--file", "12", "--type", "translate", "--base-url", ENTERPRISE_URL, "--workflow-step", "10");
+        this.execute(CommandNames.TASK, CommandNames.TASK_ADD, "\"Test Task1\"", "--language", "es", "--file", "12", "--base-url", ENTERPRISE_URL, "--workflow-step", "10");
         System.out.println("running verify command");
         verify(actionsMock)
                 .taskAdd(any(), any(), any(), any(), anyLong(), any(), anyBoolean(), anyBoolean(), any());
