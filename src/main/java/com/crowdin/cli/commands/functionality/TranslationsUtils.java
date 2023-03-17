136ae163f9f2a7e79e32d3e87af9b9b374c7123e
yzerk <zerkal.work@gmail.com>
2023-03-17 14:47:54 +0200

    fix: incorrect translation pattern on upload (windows) (#544)

--- src/main/java/com/crowdin/cli/commands/functionality/TranslationsUtils.java
+++ src/main/java/com/crowdin/cli/commands/functionality/TranslationsUtils.java
@@ -34,9 +34,8 @@ public class TranslationsUtils {
                     String[] sourceNodesTmp = sourceNodes[i].split(Utils.PATH_SEPARATOR_REGEX);
                     for (String sourceNode : sourceNodesTmp) {
                         String s = Utils.PATH_SEPARATOR + sourceNode + Utils.PATH_SEPARATOR;
-                        s = Utils.regexPath(s);
                         if (sourceFile.contains(s)) {
-                            sourceFile = sourceFile.replaceFirst(s, Utils.PATH_SEPARATOR);
+                            sourceFile = sourceFile.replaceFirst(Utils.regexPath(s), Utils.regexPath(Utils.PATH_SEPARATOR));
                         } else if (StringUtils.indexOfAny(s, new String[]{"*", "?", "[", "]", "."}) >= 0) {
                             if (sourceFile.lastIndexOf(Utils.PATH_SEPARATOR) > 0) {
                                 sourceFile = sourceFile.substring(0, sourceFile.lastIndexOf(Utils.PATH_SEPARATOR));
