698108d2285cf04745f3c1feff55f202cb0f066e
yzerk <zerkal.work@gmail.com>
2023-04-12 04:38:16 +0300

    bug: fix after testing

--- src/main/java/com/crowdin/cli/properties/PropertiesWithFilesBuilder.java
+++ src/main/java/com/crowdin/cli/properties/PropertiesWithFilesBuilder.java
@@ -88,8 +88,11 @@ public class PropertiesWithFilesBuilder extends PropertiesBuilder<PropertiesWith
             props.setPreserveHierarchy(false);
 
             Path root = Paths.get(System.getProperty("user.dir")).getRoot();
-            props.setBasePath((root != null) ? root.toString() : "/");
-            params.setSourceParam(StringUtils.removePattern(params.getSourceParam(), "^([a-zA-Z]:)?[\\\\/]+"));
+            String rootPath = root != null ? root.toString() : "/";
+            if(props.getBasePath() == null) {
+                props.setBasePath(rootPath);
+            }
+            params.setSourceParam(StringUtils.removePattern(params.getSourceParam(), "^\\?([a-zA-Z]:)?[\\\\/]+"));
         }
         if (params.getIdParam() != null) {
             props.setProjectId(params.getIdParam());
@@ -109,7 +112,7 @@ public class PropertiesWithFilesBuilder extends PropertiesBuilder<PropertiesWith
         if (params.getSourceParam() != null && params.getTranslationParam() != null) {
             FileBean fb = new FileBean();
             if (params.getSourceParam() != null) {
-                fb.setSource(StringUtils.removePattern(params.getSourceParam(), "^([a-zA-Z]:)?[\\\\/]+"));
+                fb.setSource(StringUtils.removePattern(params.getSourceParam(), "^\\?([a-zA-Z]:)?[\\\\/]+"));
             }
             if (params.getTranslationParam() != null) {
                 fb.setTranslation(params.getTranslationParam());
