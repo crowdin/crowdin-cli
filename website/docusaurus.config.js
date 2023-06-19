748bce45a1f3064dbc89229018b02b5d4913ad8b
Andrii Bodnar <andrii.bodnar@crowdin.com>
2023-06-19 16:02:38 +0300

    docs: implement search

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -42,6 +42,18 @@ const config = {
     ],
   ],
 
+  themes: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        hashed: true,
+        docsRouteBasePath: '/',
+        indexBlog: false,
+      }),
+    ]
+  ],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
