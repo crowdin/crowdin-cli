befd82704ea05445a82974bbd461bf8471106e06
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2023-10-17 00:51:16 +0530

    Runtine Exceptions assertions

--- src/test/java/com/crowdin/cli/commands/actions/PreTranslateActionTest.java
+++ src/test/java/com/crowdin/cli/commands/actions/PreTranslateActionTest.java
@@ -8,17 +8,16 @@ import com.crowdin.client.languages.model.Language;
 import com.crowdin.client.sourcefiles.model.Branch;
 import com.crowdin.client.sourcefiles.model.Directory;
 import com.crowdin.client.translations.model.PreTranslationStatus;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import com.crowdin.client.languages.model.Language;
+
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 
 import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class PreTranslateActionTest {
 
@@ -37,7 +36,6 @@ class PreTranslateActionTest {
     @Mock
     private PreTranslationStatus preTranslationStatus;
 
-    @Mock
     private PreTranslateAction action;
 
     @BeforeEach
@@ -52,7 +50,6 @@ class PreTranslateActionTest {
         when(client.downloadFullProject(anyString())).thenReturn(project);
         when(client.startPreTranslation(any())).thenReturn(preTranslationStatus);
         when(preTranslationStatus.getStatus()).thenReturn("finished");
-
         when(project.getProjectLanguages(false)).thenReturn(Collections.<Language>emptyList());
         when(project.getDirectories()).thenReturn(new HashMap<Long, Directory>());
         when(project.getBranches()).thenReturn(new HashMap<Long, Branch>());
@@ -61,7 +58,10 @@ class PreTranslateActionTest {
     void testAct_FailureWhenPreTranslationFails() {
         when(preTranslationStatus.getStatus()).thenReturn("processing", "failed");
         when(client.checkPreTranslation(anyString())).thenReturn(preTranslationStatus);
-        assertThrows(RuntimeException.class, () -> action.act(out, properties, client));
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            action.act(out, properties, client);
+        });
     }
 
     @Test
@@ -70,7 +70,10 @@ class PreTranslateActionTest {
         action = new PreTranslateAction(
                 Collections.singletonList("wrong-lang"), null, 1L, "master", null, false, false, false, false, false, false, false, null
         );
-        assertThrows(RuntimeException.class, () -> action.act(out, properties, client));
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            action.act(out, properties, client);
+        });
     }
 
     @Test
@@ -79,62 +82,57 @@ class PreTranslateActionTest {
         when(project.getDirectories()).thenReturn(new HashMap<Long, Directory>());
         when(project.getBranches()).thenReturn(new HashMap<Long, Branch>());
         when(project.getFileInfos()).thenReturn(Collections.emptyList());
-        assertThrows(RuntimeException.class, () -> action.act(out, properties, client));
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            action.act(out, properties, client);
+        });
     }
+
     @Test
-    void testPrepareLabelIds() {
+    void testPrepareLabelIds_MissingLabel() {
         action = new PreTranslateAction(
                 Collections.singletonList("en"), null, 1L, "master", null, false, false, false, false, false, false, false, Collections.singletonList("label1")
         );
         when(client.listLabels()).thenReturn(Collections.emptyList());
 
-        assertThrows(RuntimeException.class, () -> action.act(out, properties, client));
-        verify(out, times(1)).println(anyString());
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            action.act(out, properties, client);
+        });
     }
+
     @Test
-    void testPrepareFileIds() {
-        when(project.getFileInfos()).thenReturn(Collections.emptyList());
-        assertThrows(RuntimeException.class, () -> action.act(out, properties, client));
+    void testAct_AllLanguages() {
+        Language enLanguage = new Language();
+        enLanguage.setId("en");
+        when(project.getProjectLanguages(false)).thenReturn(Collections.singletonList(enLanguage));
+        action = new PreTranslateAction(
+                Collections.singletonList("all"), null, 1L, "master", null, false, false, false, false, false, false, false, null
+        );
+
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            action.act(out, properties, client);
+        });
     }
-//    @Test
-//    void testPrepareFileIds_AllSourcesExistInProjectPaths() {
-//        Language english = new Language();
-//        english.setId("en");
-//        when(project.getProjectLanguages(false)).thenReturn(Collections.singletonList(english));
-//        when(project.getDirectories()).thenReturn(new HashMap<>());
-//        when(project.getBranches()).thenReturn(new HashMap<>());
-//        when(project.getFileInfos()).thenReturn(Collections.emptyList());
-//
-//        action.act(out, properties, client);
-//
-//        verify(out, times(0)).println(anyString());
-//    }
-//
-//    @Test
-//    void testPrepareFileIds_SomeSourcesDoNotExistInProjectPaths() {
-//        Language english = new Language();
-//        english.setId("en");
-//        when(project.getProjectLanguages(false)).thenReturn(Collections.singletonList(english));
-//        when(project.getDirectories()).thenReturn(new HashMap<>());
-//        when(project.getBranches()).thenReturn(new HashMap<>());
-//        when(project.getFileInfos()).thenReturn(Collections.emptyList());
-//
-//        action.act(out, properties, client);
-//
-//        verify(out, times(1)).println(anyString());
-//    }
-//
-//    @Test
-//    void testPrepareFileIds_NoSourcesExistInProjectPaths() {
-//        Language english = new Language();
-//        english.setId("en");
-//        when(project.getProjectLanguages(false)).thenReturn(Collections.singletonList(english));
-//        when(project.getDirectories()).thenReturn(new HashMap<>());
-//        when(project.getBranches()).thenReturn(new HashMap<>());
-//        when(project.getFileInfos()).thenReturn(Collections.emptyList());
-//
-//        action.act(out, properties, client);
-//
-//        verify(out, times(1)).println(anyString());
-//    }
+
+    @Test
+    void testAct_MissingLabels() {
+        action = new PreTranslateAction(
+                Collections.singletonList("en"), null, 1L, "master", null, false, false, false, false, false, false, false, Collections.singletonList("missingLabel")
+        );
+        when(client.listLabels()).thenReturn(Collections.emptyList());
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            action.act(out, properties, client);
+        });
+    }
+
+    @Test
+    void testAct_ApplyPreTranslation() {
+        when(preTranslationStatus.getStatus()).thenReturn("processing", "finished");
+        when(client.checkPreTranslation(anyString())).thenReturn(preTranslationStatus);
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            action.act(out, properties, client);
+        });
+    }
 }
