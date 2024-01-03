a33a5a37e2c581483e0830f480e7c81df5da61e9
Andrii Bodnar <andrii.bodnar@crowdin.com>
2024-01-03 14:23:34 +0200

    build(deps): drop unused json-patch dependency (#692)

--- src/test/java/com/crowdin/cli/commands/picocli/UploadTranslationsSubcommandTest.java
+++ src/test/java/com/crowdin/cli/commands/picocli/UploadTranslationsSubcommandTest.java
@@ -2,8 +2,6 @@ package com.crowdin.cli.commands.picocli;
 
 import org.junit.jupiter.api.Test;
 
-import javax.annotation.CheckReturnValue;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.Mockito.verify;
