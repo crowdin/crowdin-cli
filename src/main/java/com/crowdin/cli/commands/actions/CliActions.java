bb57d1164f7bf7d72848d27065dfea286ac6bee5
Kateryna Oblakevych <malutina.catya14@gmail.com>
2024-02-07 16:31:08 +0200

    feat: label option for screenshot upload command (#721)

--- src/main/java/com/crowdin/cli/commands/actions/CliActions.java
+++ src/main/java/com/crowdin/cli/commands/actions/CliActions.java
@@ -253,8 +253,8 @@ public class CliActions implements Actions {
     }
 
     @Override
-    public NewAction<ProjectProperties, ClientScreenshot> screenshotUpload(File file, String branchName, String directoryPath, String filePath, boolean autoTag, boolean plainView, boolean noProgress, ProjectClient projectClient) {
-        return new ScreenshotUploadAction(file, branchName, filePath, directoryPath, autoTag, plainView, noProgress, projectClient);
+    public NewAction<ProjectProperties, ClientScreenshot> screenshotUpload(File file, String branchName, List<String> labelNames, String directoryPath, String filePath, boolean autoTag, boolean plainView, boolean noProgress, ProjectClient projectClient) {
+        return new ScreenshotUploadAction(file, branchName, labelNames, filePath, directoryPath, autoTag, plainView, noProgress, projectClient);
     }
 
     @Override
