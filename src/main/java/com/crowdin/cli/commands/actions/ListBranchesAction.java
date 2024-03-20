7d1febf2486064f6b4eefa096565f9fce4377be0
Yevheniy Oliynyk <evgen41293@gmail.com>
2024-03-20 11:56:29 +0100

    fix: updated output

--- src/main/java/com/crowdin/cli/commands/actions/ListBranchesAction.java
+++ src/main/java/com/crowdin/cli/commands/actions/ListBranchesAction.java
@@ -6,10 +6,6 @@ import com.crowdin.cli.commands.NewAction;
 import com.crowdin.cli.commands.Outputter;
 import com.crowdin.cli.properties.ProjectProperties;
 import com.crowdin.cli.utils.console.ConsoleSpinner;
-import com.crowdin.client.sourcefiles.model.Branch;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.crowdin.cli.BaseCli.RESOURCE_BUNDLE;
 import static com.crowdin.cli.utils.console.ExecutionStatus.OK;
@@ -29,17 +25,15 @@ class ListBranchesAction implements NewAction<ProjectProperties, ProjectClient> 
         CrowdinProjectFull project = ConsoleSpinner.execute(out, "message.spinner.fetching_project_info", "error.collect_project_info",
                 this.noProgress, this.plainView, client::downloadFullProject);
 
-        List<String> branchNames = project.getBranches()
-                .values()
-                .stream()
-                .map(Branch::getName)
-                .map(f -> f.replaceAll("^[/\\\\]+", ""))
-                .sorted()
-                .collect(Collectors.toList());
-        if (!plainView) {
-            branchNames.forEach(file -> out.println(OK.withIcon(String.format(RESOURCE_BUNDLE.getString("message.branch"), file))));
-        } else {
-            branchNames.forEach(out::println);
-        }
+        project
+                .getBranches()
+                .forEach((key, value) -> {
+                    String name = value.getName().replaceAll("^[/\\\\]+", "");
+                    if (!plainView) {
+                        out.println(OK.withIcon(String.format(RESOURCE_BUNDLE.getString("message.branch.list"), key, name)));
+                    } else {
+                        out.println(name);
+                    }
+                });
     }
 }
