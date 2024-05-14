f624515d8996bbdf8ef8a70b8b3f66e3a9e04ca0
Yevheniy Oliynyk <evgen41293@gmail.com>
2024-05-14 09:06:14 +0200

    fix: downgrade to java 17 (#789)

--- packages/zip/setup_crowdin.bat
+++ packages/zip/setup_crowdin.bat
@@ -1,6 +1,5 @@
 @echo off
 SETLOCAL
-SET TEMPFILE=%TEMP%\tmpfile
 
 pushd %~dp0
 
@@ -10,19 +9,39 @@ setx /M CROWDIN_HOME "%cd%"
 ECHO Adding the current directory to PATH environment variable
 setx /M PATH "%PATH%;%cd%"
 
-"%JAVA_HOME%\bin\java" -version 2>& 1 | FIND "java version" > %TEMPFILE%
-SET /p VERSIONSTRING= < %TEMPFILE%
-DEL %TEMPFILE%
-SET MAJORVERSION=%VERSIONSTRING:~14,1%
-SET MINORVERSION=%VERSIONSTRING:~16,1%
-SET UPDATEVERSION=%VERSIONSTRING:~20,-1%
+if "%_JAVACMD"=="" (
+  if not "%JAVA_HOME%"=="" (
+    if exist "%JAVA_HOME%\bin\java.exe" set "_JAVACMD=%JAVA_HOME%\bin\java.exe"
+  )
+)
+
+if "%_JAVACMD%"=="" (
+    java -version >nul 2^>^&1 && (
+        set _JAVACMD=java
+    )
+)
+
+if "%_JAVACMD%"=="" (
+    ECHO Looks like Java is not installed. You can download it from https://www.java.com/
+    GOTO EXIT
+)
+
+for /f "tokens=3" %%g in ('%_JAVACMD% -version 2^>^&1 ^| findstr /i "version"') do (
+    set JAVAVER=%%g
+)
+
+set JAVAVER=%JAVAVER:"=%
+
+for /f "delims=. tokens=1-3" %%v in ("%JAVAVER%") do (
+    set MAJORVERSION=%%v
+    set MINORVERSION=%%w
+    set UPDATEVERSION=%%x
+)
 
 ECHO Checking JAVA version
 
-IF %MAJORVERSION% GTR 1 GOTO VALID
-IF %MINORVERSION% GTR 8 GOTO VALID
-IF %UPDATEVERSION% GTR 0 GOTO VALID
-ECHO Your JAVA version should be updated to version 1.8. You can download it from https://www.java.com/
+IF %MAJORVERSION% GEQ 17 GOTO VALID
+ECHO Your JAVA version should be updated to version 17. You can download it from https://www.java.com/
 GOTO EXIT
 
 :VALID
