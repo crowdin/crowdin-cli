ec7aa0fadeffb79b102b4f3c475aebc8a23ab84f
Yevheniy Oliynyk <evgen41293@gmail.com>
2024-05-24 09:21:53 +0200

    fix: fix download output

--- src/main/java/com/crowdin/cli/commands/functionality/DryrunTranslations.java
+++ src/main/java/com/crowdin/cli/commands/functionality/DryrunTranslations.java
@@ -27,7 +27,6 @@ public class DryrunTranslations extends Dryrun {
         PropertiesWithFiles pb, LanguageMapping projectLanguageMapping, PlaceholderUtil placeholderUtil,
         List<Language> languages, boolean filesMustExist, Map<String, File> projectSources, boolean useServerSources
     ) {
-        super("message.uploading_file");
         this.pb = pb;
         this.placeholderUtil = placeholderUtil;
         this.filesMustExist = filesMustExist;
