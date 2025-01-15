aca4ab641e41cf76b454a9c97c3e40d40b33c227
Yevheniy Oliynyk <evgen41293@gmail.com>
2025-01-15 14:36:18 +0100

    fix: unit test

--- src/test/java/com/crowdin/cli/commands/picocli/FileDownloadSubcommandTest.java
+++ src/test/java/com/crowdin/cli/commands/picocli/FileDownloadSubcommandTest.java
@@ -3,6 +3,7 @@ package com.crowdin.cli.commands.picocli;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
 class FileDownloadSubcommandTest extends PicocliTestUtils {
@@ -10,14 +11,14 @@ class FileDownloadSubcommandTest extends PicocliTestUtils {
     @Test
     public void testFileDownload() {
         this.execute(CommandNames.FILE, CommandNames.FILE_DOWNLOAD, "file.txt");
-        verify(actionsMock).fileDownload(any(), any(), false, any());
+        verify(actionsMock).fileDownload(any(), any(), eq(false), any());
         this.check(true);
     }
 
     @Test
     public void testFileDownloadTranslations() {
         this.execute(CommandNames.FILE, CommandNames.FILE_DOWNLOAD, "file.txt", "--language", "uk");
-        verify(actionsMock).fileDownloadTranslation(any(), any(), any(), false, any());
+        verify(actionsMock).fileDownloadTranslation(any(), any(), any(), eq(false), any());
         this.check(true);
     }
 
