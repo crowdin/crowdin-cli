b8e93b5c6331279ae6478c296f656c1bbbabcb11
Andrii Bodnar <andrii.bodnar@crowdin.com>
2025-01-24 16:46:09 +0200

    chore: update chocolatey checksum

--- packages/chocolatey/tools/chocolateyinstall.ps1
+++ packages/chocolatey/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   url           = 'https://github.com/crowdin/crowdin-cli/releases/latest/download/crowdin-cli.zip'
-  checksum      = '07387cb2b30a4c74da037f7ab0f01b2025ca189ff333e218303f75cdf3cb73b2'
+  checksum      = '820f5c04dd0de0a1875ba8b9eae3b9f9413560914c6b6690f27255f093d2be65'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
