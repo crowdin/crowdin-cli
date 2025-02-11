2919455d9f7590e14338995a7f0a565da19b15ea
Yevheniy Oliynyk <evgen41293@gmail.com>
2025-02-11 09:37:54 +0100

    feat: print config file location (#898)

--- src/main/java/com/crowdin/cli/Cli.java
+++ src/main/java/com/crowdin/cli/Cli.java
@@ -6,13 +6,15 @@ import com.crowdin.cli.commands.picocli.CommandNames;
 import com.crowdin.cli.commands.picocli.PicocliRunner;
 import com.crowdin.cli.properties.PropertiesBuilders;
 
+import java.util.List;
+
 public class Cli {
 
     public static void main(String[] args) {
         try {
             PicocliRunner picocliRunner = PicocliRunner.getInstance();
             Actions actions = new CliActions();
-            PropertiesBuilders propertiesBuilders = new PropertiesBuilders();
+            PropertiesBuilders propertiesBuilders = new PropertiesBuilders(List.of(args).contains("--verbose") || List.of(args).contains("-v"));
             int exitCode = picocliRunner.execute(actions, propertiesBuilders, args);
             if (exitCode != -1 && picocliRunner.noneMatchArgs("--help", "--version", "plain") && args.length != 0) {
                 picocliRunner.execute(actions, propertiesBuilders, CommandNames.CHECK_NEW_VERSION);
