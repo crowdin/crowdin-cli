b213f6a25e8d9241fa44531e6845557c72f63004
Yevheniy Oliynyk <evgen41293@gmail.com>
2025-12-17 09:42:20 +0100

    fix output

--- src/main/java/com/crowdin/cli/client/Client.java
+++ src/main/java/com/crowdin/cli/client/Client.java
@@ -33,17 +33,17 @@ public interface Client {
                     S status = initialAction.get();
 
                     while (!getStatus.apply(status).equalsIgnoreCase("finished")) {
-
-                        if (progressMessage != null) {
-                            var progressMsg = RESOURCE_BUNDLE.containsKey(progressMessage) ? RESOURCE_BUNDLE.getString(progressMessage) : progressMessage;
-                            ConsoleSpinner.update(String.format(progressMsg + " (%d%%)", Math.toIntExact(getProgress.apply(status))));
-                        }
                         Thread.sleep(1000);
 
                         status = checkStatusAction.apply(status);
 
                         if (getStatus.apply(status).equalsIgnoreCase("failed")) {
                             throw new RuntimeException();
+                        }
+
+                        if (progressMessage != null) {
+                            var progressMsg = RESOURCE_BUNDLE.containsKey(progressMessage) ? RESOURCE_BUNDLE.getString(progressMessage) : progressMessage;
+                            ConsoleSpinner.update(String.format(progressMsg + " (%d%%)", Math.toIntExact(getProgress.apply(status))));
                         }
                     }
 
