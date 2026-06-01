5b6f3cd7d8d5ab82305b18ff892a67913d68f977
Davi Santos <davi.santos@liferay.com>
2026-06-01 11:36:08 -0300

    fix: prevent premature timeout when uploading large translation files

--- src/main/java/com/crowdin/cli/utils/concurrency/ConcurrencyUtil.java
+++ src/main/java/com/crowdin/cli/utils/concurrency/ConcurrencyUtil.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 public class ConcurrencyUtil {
 
     private static final int CROWDIN_API_MAX_CONCURRENT_REQUESTS = 4;
+    private static final int MIN_MINUTES_WAIT = 100;
 
     private ConcurrencyUtil() {
         throw new UnsupportedOperationException();
@@ -19,11 +20,11 @@ public class ConcurrencyUtil {
      * @param tasks list of tasks to execute in parallel
      */
     public static void executeAndWait(List<Runnable> tasks, boolean debug) {
-        run(tasks, CROWDIN_API_MAX_CONCURRENT_REQUESTS, tasks.size() * 2, debug);
+        run(tasks, CROWDIN_API_MAX_CONCURRENT_REQUESTS, Math.max(tasks.size() * 2, MIN_MINUTES_WAIT), debug);
     }
 
     public static void executeAndWaitSingleThread(List<Runnable> tasks, boolean debug) {
-        run(tasks, 1, 100, debug);
+        run(tasks, 1, MIN_MINUTES_WAIT, debug);
     }
 
     private static void run(List<Runnable> tasks, int threadQnt, int minutesWait, boolean debug) {
